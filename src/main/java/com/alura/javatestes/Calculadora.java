286eee812dd8a30472eab0ab60fae4f54fdd5b59
joanagodoy <joanapereiragodoy@gmail.com>
2022-06-21 12:22:55 -0300

    Demonstracao de um teste automatizado com alguns defeitos tais como dificuldade de conferir e execucao manual

---
+++ src/main/java/com/alura/javatestes/Calculadora.java
@@ -0,0 +1,8 @@
+package com.alura.javatestes;
+
+public class Calculadora {
+
+    public int somar(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/main/java/com/alura/javatestes/CalculadoraTeste.java
@@ -0,0 +1,21 @@
+package com.alura.javatestes;
+
+public class CalculadoraTeste {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        int soma = calc.somar(3, 7);
+        System.out.println(soma);
+
+        soma = calc.somar(3, 0);
+        System.out.println(soma);
+
+        soma = calc.somar(0, 0);
+        System.out.println(soma);
+
+        soma = calc.somar(3, -2);
+        System.out.println(soma);
+
+    }
+}
