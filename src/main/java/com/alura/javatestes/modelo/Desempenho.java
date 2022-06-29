a543c4befaacabb48abc01a44aaca30ea332d328
joanagodoy <joanapereiragodoy@gmail.com>
2022-06-29 13:55:30 -0300

    Refatora código adicionando design pattern strategy

--- src/main/java/com/alura/javatestes/modelo/Desempenho.java
+++ src/main/java/com/alura/javatestes/modelo/Desempenho.java
@@ -1,7 +1,26 @@
 package com.alura.javatestes.modelo;
 
+import java.math.BigDecimal;
+
 public enum Desempenho {
-    ADESEJAR,
-    BOM,
-    OTIMO
+    ADESEJAR {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.03");
+        }
+    },
+    BOM {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.15");
+        }
+    },
+    OTIMO {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.20");
+        }
+    };
+
+    public abstract BigDecimal percentualReajuste();
 }
--- src/main/java/com/alura/javatestes/service/ReajusteService.java
+++ src/main/java/com/alura/javatestes/service/ReajusteService.java
@@ -9,12 +9,6 @@ public class ReajusteService {
 
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if(desempenho.equals(Desempenho.ADESEJAR)){
-            funcionario.aplicaReajuste(new BigDecimal("0.03"));
-        }else if(desempenho.equals(Desempenho.BOM)){
-            funcionario.aplicaReajuste(new BigDecimal("0.15"));
-        }else if(desempenho.equals(Desempenho.OTIMO)){
-            funcionario.aplicaReajuste(new BigDecimal("0.20"));
-        }
+        funcionario.aplicaReajuste(desempenho.percentualReajuste());
     }
 }
